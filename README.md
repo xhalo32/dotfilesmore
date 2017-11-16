9af68a5dbdc88c4c75cb3724d034a220d233de01
Niklas Halonen <kahvikannu@gmail.com>
2017-11-16 14:05:14 +0200

    vimrc added, updated README

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # This is my config repository
 
 Here lies:
-- $HOME/bin
 - $HOME/.vimrc
+- $HOME/.bashrc
+- $HOME/.gitignore
+
+Here will lie:
+- $HOME/bin
--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 echo "Installing to $HOME"
+
+echo ".bashrc"
 ln -f .bashrc $HOME/.bashrc
+echo ".gitignore"
 ln -f .gitignore $HOME/.gitignore
+echo ".vimrc"
+ln -f .vimrc $HOME/.vimrc
 
 git config --global core.excludesfile "$HOME/.gitignore"
